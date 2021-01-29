4e8a340a10cf94ae2cfe24b6e7a9b2aea08a3b1b
Georgi Paskalev <gpaskalev@gmail.com>
2021-01-29 03:34:20 +0200

    Lecture3_Task9

---
+++ l3_t9.c
@@ -0,0 +1,22 @@
+/* Напишете функция, която получава като параметър число
+и два индекса k и n и връща числото със занулени битове
+между позиция k и n.*/
+
+#include <stdio.h>
+int removePoz(unsigned int, int, int);
+    
+int main(){
+    printf("%d\n",removePoz(126, 2, 5));
+    return 0;
+}       
+int removePoz(unsigned int nNumber, int index1, int index2){
+    int i = 1;
+    int mask;
+    -- index2;
+    while(index1 < index2){
+        mask = i << index2;
+        nNumber = nNumber & ~mask;
+        -- index2;
+    };
+    return nNumber;
+}
